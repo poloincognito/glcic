ff1753ea5b60ea963d202f2da7696d7c9e16cb7c
emerubis <paulguillermit@gmail.com>
2023-09-18 15:48:08 +0200

    Implement masking

--- lib/glic/preprocessing/preprocessing_tools.py
+++ lib/glic/preprocessing/preprocessing_tools.py
@@ -8,15 +8,16 @@ def generate_mask(batch_size: int) -> tuple:
     It first generates a 128*128 mask, that will be used by the context discriminator,
     and then generates a submask whose height and width are randomly selected from [96, 128].
     """
-    local_mask = torch.zeros((batch_size, 256, 256), dtype=torch.bool)
+    local_mask = []
     erase_mask = torch.zeros((batch_size, 256, 256), dtype=torch.bool)
     for i in range(batch_size):
         # local mask
-        w, h = np.random.randint(0, 128), np.random.randint(0, 128)
-        local_mask[i, h : h + 128, w : w + 128] = True
+        w0, h0 = np.random.randint(0, 128), np.random.randint(0, 128)
+        local_mask.append([h0, h0 + 128, w0, w0 + 128])
         # random submask
-        subw, subh = np.random.randint(0, 32), np.random.randint(0, 32)
-        local_mask[i, h + subh : h + 128, w + subw : w + 128] = True
+        w, h = np.random.randint(96, 128), np.random.randint(96, 128)
+        w1, h1 = np.random.randint(0, 128 - w), np.random.randint(0, 128 - h)
+        erase_mask[i, h0 + h1 : h0 + h, w0 + w1 : w0 + w] = True
     return local_mask, erase_mask
 
 
