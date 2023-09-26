84375743797cbe5c4a61969912e72fb166a0fa0d
emerubis <paulguillermit@gmail.com>
2023-09-26 14:13:29 +0200

    Rename glic glcic

--- lib/setup.py
+++ lib/setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="glic",
+    name="glcic",
     packages=find_packages(),
     version="0.1.0",
     description="My image completion library",
